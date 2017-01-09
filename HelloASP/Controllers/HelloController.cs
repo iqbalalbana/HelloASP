8b83c69c394ca4a53c192e58b85258539b5b5435
Iqbal Albana <Iqbal Albana@DESKTOP-D1VVTJS>
2017-01-09 14:02:05 +0700

    Add project files.

---
+++ HelloASP/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloASP.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult view()
+        {
+            return View();
+        }
+    }
+}
